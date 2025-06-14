e7fab20d7f21a7d7b5ea9344f3e1c7790082c9f8
MariaritaPatrelli <mariaritapatrelli@gmail.com>
2025-06-14 13:27:59 +0200

    Initial Commit

---
+++ Still-Tasty/Still_TastyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Still_TastyApp.swift
+//  Still-Tasty
+//
+//  Created by Mariarita Patrelli on 14/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Still_TastyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Still-TastyTests/Still_TastyTests.swift
@@ -0,0 +1,17 @@
+//
+//  Still_TastyTests.swift
+//  Still-TastyTests
+//
+//  Created by Mariarita Patrelli on 14/06/25.
+//
+
+import Testing
+@testable import Still_Tasty
+
+struct Still_TastyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
